c2e488d44c5982152d6b1ebfbf7a29010cc538e2
Saikiran Belana <saikiran.belana@uconn.edu>
2024-03-22 10:00:02 -0400

    fix query_combos re-updations + append possible values

--- Projects/Project2/single-server/server.py
+++ Projects/Project2/single-server/server.py
@@ -9,18 +9,15 @@ server_socket = socket(AF_INET, SOCK_STREAM) # SO_REUSE_ADDR helps in rebindin
 server_socket.bind((myHost, myPort))
 server_socket.listen(5)
 
-# print(string.ascii_lowercase)
-
-# for x in WordList:
-    # print(x) # read line by line
-
 
 def now(): #This function provides the time stamp when called
     return time.ctime(time.time())
 
 #  Our Recursive function to call itself again and again until all fields are satisfied to search the query
-def searchQuery(query, request, missing_query_locations, query_combos):
-    print("searchn query called with ", query)
+def searchQuery(request):
+    print("search query called with ", request)
+    query_combos = [] # storing all the searchable names [A-Z]
+    missing_query_locations = [] # Know the no. of '?' provided in query - we store the indices of '?'
 
     if "?" in request: # check if '?' available in request
         for index in range(len(request)):
@@ -29,26 +26,38 @@ def searchQuery(query, request, missing_query_locations, query_combos):
         
         # Add all searchable words for the missing characters to query_combos
         for location in range(len(missing_query_locations)):
-            for alphabet in string.ascii_lowercase: # add all letters for search
-
+                print("query combo:  ", query_combos)
                 if location == 0:
-                    searchable = list(request) #convert request to list
-                    searchable[missing_query_locations[location]] = alphabet
-                    searchable = ''.join(searchable)
-                    # print("Request Searchable: ", searchable)
-                    request_searchable = searchable
-                    # update and re-update for multiple '?'
-                    query_combos.append(request_searchable)
+
+                    for alphabet in string.ascii_lowercase: # add all letters for search
+
+                        searchable = list(request) #convert request to list
+                        searchable[missing_query_locations[location]] = alphabet
+                        searchable = ''.join(searchable) #remove quotes and join the letters
+                        # print("Request Searchable: ", searchable)
+                        request_searchable = searchable
+                        # update and re-update for multiple '?'
+                        query_combos.append(request_searchable)
+
 
                 else:
-                    for query_index in range(len(query_combos)):
-                        searchable = list(query_combos[query_index]) #convert request to list
-                        searchable[missing_query_locations[location]] = alphabet
-                        searchable = ''.join(searchable)
-                        # print("Request Searchable: ", searchable)
-                        query_combos[query_index] = searchable
-    return query_combos, missing_query_locations
+                    temp_combos = query_combos.copy() # create a copy of query_combos
+                    query_combos = [] # empty the query_combos
+                    for query in temp_combos:
+                        for alphabet in string.ascii_lowercase:
+                            searchable = list(query) # convert request word to list of characters
+                            searchable[missing_query_locations[location]] = alphabet
+                            searchable = ''.join(searchable)
+                            query_combos.append(searchable) # append new string to query_combos
 
+
+    else:
+        query_combos.append(request)
+    return query_combos
+
+
+# result = searchQuery("a?a?m")
+# print("Result: ", result)
 
 
 def handleRequest(request): # function for handling the request
@@ -59,39 +68,7 @@ def handleRequest(request): # function for handling the request
     response_data = '' #empty response
     found = False # default is False
     
-    missing_query_locations = [] # Know the no. of '?' provided in query - we store the indices of '?'
-    query_combos = [] # storing all the searchable names [A-Z]
-
-    if "?" in request: # check if '?' available in request
-        for index in range(len(request)):
-            if "?" == request[index]:
-                missing_query_locations.append(index)
-        
-        # Add all searchable words for the missing characters to query_combos
-        for location in range(len(missing_query_locations)):
-            for alphabet in string.ascii_lowercase: # add all letters for search
-
-                if location == 0:
-                    searchable = list(request) #convert request to list
-                    searchable[missing_query_locations[location]] = alphabet
-                    searchable = ''.join(searchable)
-                    # print("Request Searchable: ", searchable)
-                    request_searchable = searchable
-                    # update and re-update for multiple '?'
-                    query_combos.append(request_searchable)
-
-                else:
-                    for query_index in range(len(query_combos)):
-                        searchable = list(query_combos[query_index]) #convert request to list
-                        searchable[missing_query_locations[location]] = alphabet
-                        searchable = ''.join(searchable)
-                        # print("Request Searchable: ", searchable)
-                        query_combos[query_index] = searchable
-
-    else:
-        query_combos.append(request)
-    
-    print("len(missing_query_locations): ", len(missing_query_locations))
+    query_combos = searchQuery(request)
     print("Query Combos to search: ", query_combos)
 
     for word in WordList:
@@ -134,3 +111,5 @@ def dispatcher():
         handleClient(connection)
 
 dispatcher()
+if __name__ == '__main__':
+    dispatcher()    # call the dispatcher function
